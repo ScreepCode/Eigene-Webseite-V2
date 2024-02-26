7cc4b194d7230b35ba26241aaeb5c5a1a7627efa
Niklas Buse <nicki_bu@gmx.de>
2024-02-26 21:30:04 +0100

    New: /now Page

---
+++ static/js/lastUpdate.js
@@ -0,0 +1,18 @@
+// Store dates in variables
+const lastEdited = new Date("2024-02-19");
+const today = new Date();
+
+// Calculate the difference in milliseconds
+const deltaMs = today - lastEdited;
+
+// Convert the delta to days
+const deltaDays = Math.floor(deltaMs / (1000 * 60 * 60 * 24));
+
+const lastEditedInfoElement = document.getElementById("last-edited-info");
+// Print the result
+lastEditedInfoElement.innerHTML = `
+Am ${lastEdited.toLocaleDateString("de-DE")} (${deltaDays} Tage her)
+<br>
+aus meiner Wohnung in Bruchsal
+<br>
+`;
